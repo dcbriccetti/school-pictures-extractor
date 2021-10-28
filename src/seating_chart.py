7cd865957c1e5688c443a3767e2de5346e99ff90
Dave Briccetti <daveb@davebsoft.com>
2021-10-27 18:10:49 -0700

    Better organize

--- src/seating_chart.py
+++ src/seating_chart.py
@@ -7,6 +7,8 @@ from station import Station
 
 FONT_FILE = os.environ['FONT_FILE']  # a font usable with ImageDraw
 PICTURES_DIR = os.environ['PICTURES_DIR']  # directory with pictures, with subdirectories for grades
+INPUTS_DIR = 'seating-chart-inputs'
+OUTPUTS_DIR = 'seating-chart-outputs'
 GRADES = 6, 7, 8
 TEXT_MARGIN = 6
 
@@ -16,7 +18,7 @@ def file_lines(filename) -> list[str]:
 
 def station_empty(student: str) -> bool: return not student
 
-stations = [Station.from_string(line) for line in file_lines('station coordinates.txt')]
+stations = [Station.from_string(line) for line in file_lines(f'{INPUTS_DIR}/station coordinates.txt')]
 font = ImageFont.truetype(FONT_FILE, 24)
 text_color = (255, 255, 255)
 
@@ -30,9 +32,12 @@ def draw_text(index: int, student: str, image_pos: np.ndarray, image_width: int)
 
     draw.text(station_number_loc, station_number_str, fill=text_color, font=font)
 
+if not os.path.isdir(OUTPUTS_DIR):
+    os.mkdir(OUTPUTS_DIR)
+
 for grade in GRADES:
     grade_pictures_dir = f'{PICTURES_DIR}/{grade}'
-    students = file_lines(f'students{grade}.txt')
+    students = file_lines(f'{INPUTS_DIR}/students{grade}.txt')
 
     with Image.open('room.jpg') as img:
         draw = ImageDraw.Draw(img)
@@ -49,4 +54,4 @@ for grade in GRADES:
                 img.paste(stu_img, tuple(image_top_left))  # Add the student picture
                 draw_text(index, student, image_top_left, stu_img.width)  # Add the name and station #
 
-        img.save(f'seating chart {grade}.jpg')
+        img.save(f'{OUTPUTS_DIR}/seating chart {grade}.jpg')
