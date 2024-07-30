6e4a612f72edff7aa03f24515006db66649982b4
Dave Briccetti <daveb@davebsoft.com>
2024-07-29 18:20:02 -0700

    Change to one page at a time, and a whole class on one page

--- src/extractor.py
+++ src/extractor.py
@@ -3,43 +3,49 @@ import pytesseract
 from glob import glob
 import os
 
-PICS_PER_PAGE = 8
-INTER_PIC_V_SPACING = 254.5
-PIC_LEFT_X = 1020
-PIC_WIDTH = 272
-PIC_HEIGHT = 203
-FIRST_PIC_Y = 75.5
-NAME_Y_OFFSET_FROM_PIC_Y = 29
-NAME_X = 1344
-GRADES = 6, 7, 8
-PAGES_PER_GRADE = 4
+NUM_ROWS = 4
+NUM_COLS = 7
+TOP_LEFT = 87, 582
+PIC_DIMS = 190, 235
+SLOP = 3  # to avoid the border
+PIC_SPACING = 212, 343
+NAME_AREA_HEIGHT = PIC_SPACING[1] - PIC_DIMS[1] - 10
+NAME_Y_OFFSET_FROM_PIC_Y = PIC_DIMS[1] + 8
 OUTPUT_DIR = 'pics/extracted'
+SHOW_IMAGES = False
 
-def make_directories():
-    def md(dir: str) -> None:
-        if not os.path.isdir(dir):
-            os.mkdir(dir)
-
-    md(f'{OUTPUT_DIR}')
-    for grade in GRADES:
-        md(f'{OUTPUT_DIR}/{grade}')
-
-make_directories()
+if not os.path.isdir(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
 
 files = glob('pics/SCAN*.JPG')
 files.sort()
-for page_index, fn in enumerate(files):
-    grade_dir = GRADES[(page_index // PAGES_PER_GRADE)]
-    print(fn, grade_dir)
+for fn in files:
+    print(fn)
     with Image.open(fn) as im:
-        for i in range(PICS_PER_PAGE):
-            y = FIRST_PIC_Y + i * INTER_PIC_V_SPACING
-
+        for yi in range(NUM_ROWS):
+            y = TOP_LEFT[1] + yi * PIC_SPACING[1]
             name_y = y + NAME_Y_OFFSET_FROM_PIC_Y
-            name_image = im.crop((NAME_X, name_y, NAME_X + 352, name_y + 40))
-            if name := pytesseract.image_to_string(name_image).strip():
-                cropped = im.crop((PIC_LEFT_X, y, PIC_LEFT_X + PIC_WIDTH, y + PIC_HEIGHT))
-                rotated = cropped.transpose(Image.ROTATE_270)
-                # rotated.show()
-                print(name)
-                rotated.save(f'{OUTPUT_DIR}/{grade_dir}/{name}.jpg')
+
+            for xi in range(NUM_COLS):
+                x = TOP_LEFT[0] + xi * PIC_SPACING[0]
+                name_image = im.crop((
+                    x,
+                    name_y,
+                    x + PIC_DIMS[0],
+                    name_y + NAME_AREA_HEIGHT
+                ))
+                if SHOW_IMAGES:
+                    name_image.show()
+
+                if raw_name := pytesseract.image_to_string(name_image).strip():
+                    name = raw_name.replace('\n', ' ')
+                    print(name)
+                    cropped = im.crop((
+                        x,
+                        y,
+                        x + PIC_DIMS[0] - SLOP,
+                        y + PIC_DIMS[1]
+                    ))
+                    if SHOW_IMAGES:
+                        cropped.show()
+                    cropped.save(f'{OUTPUT_DIR}/{name}.jpg')
