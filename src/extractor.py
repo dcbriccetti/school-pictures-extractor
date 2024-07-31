22c83f55cac407440710b54f5fd08bf3e0b29b6a
Dave Briccetti <daveb@davebsoft.com>
2024-07-30 23:45:01 -0700

    Improve readability

--- src/extractor.py
+++ src/extractor.py
@@ -15,21 +15,16 @@ NAME_Y_OFFSET_FROM_PIC_Y = PIC_DIMS[1] + 6
 OUTPUT_DIR = 'pics/extracted'
 SHOW_IMAGES = True
 
-if not os.path.isdir(OUTPUT_DIR):
-    os.mkdir(OUTPUT_DIR)
 
-files = glob('pics/SCAN*.JPG')
-files.sort()
-for fn in files:
-    print(fn)
-    with Image.open(fn) as im:
+def process_page(fn):
+    with Image.open(fn) as page_image:
         for yi in range(NUM_ROWS):
             y = TOP_LEFT[1] + yi * PIC_SPACING[1]
             name_y = y + NAME_Y_OFFSET_FROM_PIC_Y
 
             for xi in range(NUM_COLS):
                 x = TOP_LEFT[0] + xi * PIC_SPACING[0]
-                name_image = im.crop((
+                name_image = page_image.crop((
                     x,
                     name_y,
                     x + PIC_DIMS[0],
@@ -41,7 +36,7 @@ for fn in files:
                 if raw_name := pytesseract.image_to_string(name_image).strip():
                     name = re.sub(r'\n+', ' ', raw_name)
                     print(name)
-                    cropped = im.crop((
+                    cropped = page_image.crop((
                         x,
                         y,
                         x + PIC_DIMS[0] - SLOP,
@@ -50,3 +45,11 @@ for fn in files:
                     if SHOW_IMAGES:
                         cropped.show()
                     cropped.save(f'{OUTPUT_DIR}/{name}.jpg')
+
+
+if not os.path.isdir(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
+
+for fn in glob('pics/SCAN*.JPG'):
+    print(fn)
+    process_page(fn)
